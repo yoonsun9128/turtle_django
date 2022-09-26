d3f1515dd39a8071203f2b42f925b7083193d9ca
yoonsun9128 <yoonsun9128@gmail.com>
2022-09-26 10:52:23 +0900

    commit

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,15 @@
+import random
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def dinner(request,name):
+    menus = ['조발', '햄버거' ,'피자','치킨','초밥','닭발']
+    pick = random.choice(menus)
+    context = {
+        'pick':pick,
+        'name':name,
+    }
+    return render(request, 'dinner.html', context)
