4f420a3369cb42b5dd6c13e0a6c82cb694373b7d
yoonsun9128 <yoonsun9128@gmail.com>
2022-09-30 01:14:46 +0900

    articles models

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Models): #상속
+    # id 는 자동으로 만들어진다.
+    title = models.CharField(max_length=10)
+    content = models.TextField()
+    create_at = models.DateTimeField(auto_now_add=True)
+
